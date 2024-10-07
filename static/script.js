0b249d500bf671a6adcf013ee01c2fc2d1222bd4
lwang-8k <800026388@fjuhsd.org>
2024-10-07 20:35:48 +0000

    changed v to 1 2 3

--- static/script.js
+++ static/script.js
@@ -1,1 +1,3 @@
-console.log("Hello from the console!")
+
+//DONT TOUCH THIS PART
+const exec=id=>{ let script = document.createElement("script"); script.src = id+".js"; document.body.appendChild(script);}
